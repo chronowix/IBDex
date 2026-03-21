c1d8f3b7654a54c5306a4ba2ad2fbf15e999d26e
chronowix <a.garcia.fern05@gmail.com>
2026-03-21 11:35:02 +0100

    2nd init of project

---
+++ lib/src/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../features/home/presentation/home_page.dart';
+
+/// Le point d'entrée principal des widgets de l'application.
+/// Définit le thème global et la navigation racine.
+class IbdexApp extends StatelessWidget {
+  const IbdexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Définir ici le MaterialApp, le thème global et la page d'accueil
+    // Utiliser HomePage() comme 'home' du MaterialApp
+    return const Placeholder();
+  }
+}
---
+++ lib/src/features/home/presentation/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// La page d'accueil de l'application IBDEX.
+/// Présente une introduction à la vulgarisation des MICI.
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implémenter le Scaffold avec un AppBar (titre "IBDEX") 
+    // et un corps présentant le but de l'application.
+    return const Placeholder();
+  }
+}
