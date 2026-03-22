638c1e2cac72c80010dfc2783ade3585e2f6c7f6
chronowix <a.garcia.fern05@gmail.com>
2026-03-22 16:20:08 +0100

    added navbar and put footer content in burger menu

--- lib/src/features/home/presentation/home_page.dart
+++ lib/src/features/home/presentation/home_page.dart
@@ -1,46 +1,59 @@
 import 'package:flutter/material.dart';
 
-// La page d'accueil de l'application IBDEX.
-// Présente une introduction à la vulgarisation des MICI.
-class HomePage extends StatelessWidget {
+//Page d'accueil
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+  @override State<HomePage> createState() =>
+      _HomePageState(); //création de l'état
+}
+
+class _HomePageState extends State<HomePage>{
+  int _selectedIndex = 0; // quel onglet est actif
+  //liste des pages
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('Articles')),
+    Center(child: Text('Wiki')),
+    Center(child: Text('Conseils')),
+    Center(child: Text('Nutrition')),
+    Center(child: Text('Carte')),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Implémenter le Scaffold avec un AppBar (titre "IBDEX")
-    // et un corps présentant le but de l'application.
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('IBDex'),
       ),
-      drawer: Drawer(
-        child: ListView(
+      drawer: Drawer( //menu burger
+        child: Column(
           children: [
             const DrawerHeader(child: Text('Menu IBDex')),
             ListTile(title: const Text('Mon Profil')),
-            ListTile(title: const Text('A Propos')),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hello World!')
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.blue,
-        child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              '© 2026 IBDEX - Tous droits réservés',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
+            ListTile(title: const Text('Paramètres')),
+            const Spacer(),
+            const Padding(
+                padding: EdgeInsets.all(16.0),
+              child: Text('© 2026 IBDEX - Tous droits réservés', style: TextStyle(fontSize: 12)),
             ),
-        )
+          ],
+        ),
+      ),
+      body: _widgetOptions[_selectedIndex], //affichage de la page sélectionnée
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          onTap: (int index){
+            setState(() {
+              _selectedIndex = index; //maj de l'index
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.article), label: 'Articles'),
+            BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'Wiki'),
+            BottomNavigationBarItem(icon: Icon(Icons.lightbulb), label: 'Conseils'),
+            BottomNavigationBarItem(icon: Icon(Icons.restaurant), label: 'Nutrition'),
+            BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Carte'),
+          ],
       ),
     );
   }
